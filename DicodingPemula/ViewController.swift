1f5a5f4fad8837066eefb4611ba292699c3b892b
sayyidmaulana <sayyid.yakin@students.amikom.ac.id>
2020-04-14 17:48:42 +0700

    Initial Commit

---
+++ DicodingPemula/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DicodingPemula
+//
+//  Created by Sayyid Maulana Khakul Y on 14/04/20.
+//  Copyright © 2020 Dicoding. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
